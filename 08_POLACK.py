eea0b9b17ba00718f892937ed8cc5ee13b88331e
Fraaaaaaaaaaaaaaaaaaaan <frmuuoo@gmail.com>
2026-04-16 20:57:26 -0300

    Progreso en el ejercicio 08

--- 08_POLACK.py
+++ 08_POLACK.py
@@ -3,6 +3,9 @@ class Personaje:
     def __init__(self,ataque,curar):
         self.ataque=ataque
         self.curar=curar
+    def curamiento(self):
+        self.curamiento=self.vida+self.curar
+    
 
 usuario=Personaje(5,3)
 print(usuario.curar,usuario.ataque,usuario.vida)
@@ -11,16 +14,26 @@ print(enemigo.vida,enemigo.curar,enemigo.ataque)
 
 menu=True
 while menu:
-    print("op1,op2,op3")
+    print("1)Consultar tu estado")
+    print("2)Atacar al enemigo")
+    print("3)Curarse")
+    print("4)Abandonar el duelo")
     op=int(input("Elija la opcion de menu que desea realizar:"))
     if op==1:
-            print("x")
+        print("x")
     elif op==2:
-            print("y")
+        print(f"Atacas al enemigo y le haces {usuario.ataque} de daño!!")
+            
     elif op==3:
-          print("z")
+        print("Curando...")
+        usuario.curamiento()
+        print(f"Vida:{usuario.vida}/{Personaje.vida}")
+        print("El enemigo responde...")
+        if enemigo.vida<10:
+            print("Curandose...")
+            print(f"Vida del enemigo al")
     elif op==4:
-        print("fin")
+        print("GAME OVER. Abandonaste el duelo!!")
         menu=False
         
 
