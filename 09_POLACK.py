67920e18839bdccbb6a7089a639b3a346cb6d08d
Fraaaaaaaaaaaaaaaaaaaan <frmuuoo@gmail.com>
2026-04-16 21:28:44 -0300

    Comienzo del ejercicio 09

---
+++ 09_POLACK.py
@@ -0,0 +1,10 @@
+Ejercicio 9: EL TESORO ESCONDIDO
+Un explorador se adentra en una cueva en busca de tesoros. En su camino encuentra cofres que pueden estar
+cerrados o abiertos, y que contienen cierta cantidad de monedas.
+El usuario deberá decidir qué hacer en cada momento: intentar abrir cofres, recolectar monedas o continuar
+explorando.
+El sistema debe permitir visualizar el estado del cofre y la cantidad de recursos acumulados por el jugador
+hasta finalizar la exploración.
+from random import randint
+class Cofre:
+    
