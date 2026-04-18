99ccc083694fedc96819c1c34de5405bc0288ba2
Fraaaaaaaaaaaaaaaaaaaan <frmuuoo@gmail.com>
2026-04-18 10:21:58 -0300

    Implementado codigo principal

--- 11_POLACK.py
+++ 11_POLACK.py
@@ -1,7 +1,3 @@
-Ejercicio 11: BATALLA DE HECHICEROS
-cada acción tiene consecuencias sobre los recursos disponibles del personaje, por lo que será necesario
-administrarlos correctamente.
-El enfrentamiento continúa hasta que uno de los participantes ya no pueda seguir luchando.
 
 class Hechicero:
     def __init__(self,salud,mana,hechizo_1,hechizo_2,danio1,danio2):
@@ -13,6 +9,7 @@ class Hechicero:
         self.danio2=danio2
     def curar(self):
         self.salud=self.salud+self.mana
+        self.mana-=5
     
     def hechizo_uno(self):
         print(f"Hechizo {self.hechizo_1} ejecutado!!")
@@ -21,14 +18,13 @@ class Hechicero:
         self.salud-=1
 
     def hechizo_dos(self):
-        daniomana=self.danio2//2
         print(f"Hechizo {self.hechizo_2} ejecutado!!")
         print(f"Lidia un {self.danio2} de daño")
-        print(f"Pero resta un {daniomana} al MANA")
-        self.mana-=daniomana
+        print(f"Pero resta un mitad al MANA")
+        self.mana=self.mana//2
 
 usuario_salud=15
-usuario_mana=5
+usuario_mana=10
 usuario_hechizo_1="Abracadabra"
 usuario_danio_1=10
 usuario_hechizo_2="Pata de Cabra"
@@ -37,9 +33,9 @@ usuario_danio_2=20
 enemigo_salud=15
 enemigo_mana=5
 enemigo_hechizo_1="Hocus Pocus"
-enemigo_danio_1=10
+enemigo_danio_1=20
 enemigo_hechizo_2="Pata de Rana"
-enemigo_danio_2=20  
+enemigo_danio_2=30  
 
 usuario=Hechicero(usuario_salud,usuario_mana,usuario_hechizo_1,usuario_hechizo_2,usuario_danio_1,usuario_danio_2)
 enemigo=Hechicero(enemigo_salud,enemigo_mana,enemigo_hechizo_1,enemigo_hechizo_2,enemigo_danio_1,enemigo_danio_2)
@@ -47,9 +43,64 @@ enemigo=Hechicero(enemigo_salud,enemigo_mana,enemigo_hechizo_1,enemigo_hechizo_2
 print("Bienvenido a la BATALLA de HECHICEROS")
 batalla=True
 while batalla:
-    print("\n--------------------------------------------")
-    print(f"1)ATACAR con HECHIZO 1:{usuario_hechizo_1}:Lidia 10 de DAÑO al enemigo pero resta -2 a la SALUD")
-    print(f"1)ATACAR con HECHIZO 2:{usuario_hechizo_2}:Lidia ")
+    if usuario.salud==0:
+        print("game over")
+    elif enemigo.salud==0:
+        print("")
+    else:
+        print("\n--------------------------------------------")
+        print(f"1)ATACAR con HECHIZO 1:{usuario_hechizo_1}:Lidia 10 de DAÑO al enemigo pero resta -2 a la SALUD")
+        print(f"2)ATACAR con HECHIZO 2:{usuario_hechizo_2}:Lidia 20 de DAÑO al enemigo pero resta deja a la mitad al MANA")
+        print(f"3)USAR MANA: Cura {usuario_mana} a la SALUD")
+        print("4)CONSULTAR mi ESTADO")
+        print("5)ABANDONAR la BATALLA")
+        print("--------------------------------------------")
+        op=int(input("\nTu turno!! Ingrese la accion que desea realizar:"))
+        if op==1:
+            print("\n--------------------------------------------")
+            enemigo.salud-=usuario_hechizo_1
+            usuario.hechizo_uno()
+            print("Accion")
+            print("Tu enemigo responde...")
+            usuario.salud-=enemigo_hechizo_2
+            enemigo.hechizo_dos()
+            print(f"hechizo uno te lidia {enemigo_danio_2} de daño")
+            print("--------------------------------------------")
+        elif op==2:
+            print("\n--------------------------------------------")
+            enemigo.salud-=usuario_hechizo_2
+            usuario.hechizo_dos()
+            print("Accion")
+            print("Tu enemigo responde...")
+            enemigo.curar()
+            print(f"Curandose,tiene ahora {enemigo.salud} de vida")
+            print("--------------------------------------------") 
+        elif op==3:
+            print("\n--------------------------------------------")
+            usuario.curar()
+            print("Accion")
+            print("Tu enemigo responde...")
+            usuario.salud-=enemigo_hechizo_1
+            enemigo.hechizo_uno()
+            print(f"hechizo uno te lidia {enemigo_danio_1} de daño")
+            print("--------------------------------------------")
+        elif op==4:
+            print("\n--------------------------------------------")
+            print(f"SALUD:{usuario.salud}/{usuario_salud}")
+            print(f"MANA:{usuario.mana}/{usuario_mana}")
+            print(f"CURACIONES RESTANTES:{usuario.mana}")
+            print("--------------------------------------------")
+        elif op==5:
+            print("\n--------------------------------------------")
+            print("GAME OVER")
+            print("Has abandonado la batalla de hechiceros :(")
+            print("--------------------------------------------")
+        else:
+            print("Ingrese una opcion del menu")
+
+    
+
+
 
     
 
