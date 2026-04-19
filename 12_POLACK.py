f95dcf0dfd824e9df1896eda6a6ba9a5fc611f95
Fraaaaaaaaaaaaaaaaaaaan <frmuuoo@gmail.com>
2026-04-19 19:42:55 -0300

    Creado el archivo

---
+++ 12_POLACK.py
@@ -0,0 +1,5 @@
+Ejercicio 12: AVENTURA Y OBJETOS
+Un aventurero recorre distintos escenarios recolectando objetos que encuentra en su camino. El usuario podrá
+decidir qué hacer con cada objeto: guardarlo, utilizarlo o descartarlo. Además, deberá poder consultar en todo
+momento qué elementos posee.
+El programa debe permitir gestionar esta información de forma dinámica durante la ejecución.
